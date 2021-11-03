1f6f62124744383fc2f58b3256b29c1d9c51de1b
ziedlassoued <zied.lassoued@outlook.de>
2021-11-03 16:30:41 +0100

    :recycle: Add SplashImage component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import styles from './App.module.css';
 import Title from './components/Title';
+import SplashImage from './components/Splashimage/Splashimage';
 
 function App(): JSX.Element {
   return (
     <main className={styles.container}>
-      <img
-        src="https://images.unsplash.com/photo-1515169067868-5387ec356754?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1770&q=80"
+      <SplashImage
+        url="https://images.unsplash.com/photo-1515169067868-5387ec356754?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1770&q=80"
         alt="Bergfest"
       />
       <div className={styles.welcome}>
