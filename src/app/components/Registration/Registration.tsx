e7c6ca26132b7ae77f4816e935d558695196df29
ziedlassoued <zied.lassoued@outlook.de>
2021-11-04 12:01:06 +0100

    :sparkles: Load users onSelectClick

--- src/app/components/Registration/Registration.tsx
+++ src/app/components/Registration/Registration.tsx
@@ -1,9 +1,15 @@
 import React, { ChangeEvent, FormEvent, useState } from 'react';
 import styles from './Registration.module.css';
 
+type User = {
+  firstName: string;
+  lastName: string;
+};
+
 function Registration(): JSX.Element {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
+  const [users, setUsers] = useState<User[]>([]);
 
   function handleSubmit(event: FormEvent) {
     event.preventDefault();
@@ -28,23 +34,39 @@ function Registration(): JSX.Element {
     setLastName(event.target.value);
   }
 
+  async function handleSelectClick() {
+    const response = await fetch('https://json-server.machens.dev/users');
+    const newUsers = await response.json();
+    setUsers(newUsers);
+  }
+
+  const userOptions = users.map((user) => (
+    <option>
+      {user.firstName} {user.lastName}
+    </option>
+  ));
+
   return (
     <form className={styles.form} onSubmit={handleSubmit}>
+      <select onClick={handleSelectClick}>
+        <option disabled>Select user</option>
+        {userOptions}
+      </select>
+      or
       <input
         type="text"
-        placeholder="Firstname"
+        placeholder="First name"
         value={firstName}
         onChange={handleFirstNameChange}
       />
       <input
         type="text"
-        placeholder="Lastname"
+        placeholder="Last name"
         value={lastName}
         onChange={handleLastNameChange}
       />
-      <input type="submit" value="Add +" />
+      <input type="submit" value="Sign in" />
     </form>
   );
 }
-
 export default Registration;
